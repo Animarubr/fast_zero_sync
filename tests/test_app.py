30321a14160ea7384e0ec092f5bd6f9432db3844
Marcos Vinicius <animarubr@gmail.com>
2024-06-28 11:28:37 -0300

    Atualizando endpoints para usar o banco de dados real, atualizado testes e exercícios

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,7 @@
 from http import HTTPStatus
 
+from fast_zero.schemas import UserPublic
+
 
 def test_read_root_deve_retornar_ok_e_ola_mundo(client):
     response = client.get('/')  # Act (Ação)
@@ -25,21 +27,48 @@ def test_create_user(client):
     }
 
 
+def test_create_user_fail_username(client, user):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'test',
+            'password': '1236',
+            'email': 'test@test.com',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+    assert response.json() == {'detail': 'Username already exists.'}
+
+
+def test_create_user_fail_email(client, user):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'teste',
+            'password': '1236',
+            'email': 'test@test.com',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+    assert response.json() == {'detail': 'Email already exists.'}
+
+
 def test_read_users(client):
     response = client.get('/users/')
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {
-        'users': [
-            {
-                'id': 1,
-                'username': 'test',
-                'email': 'test@test.com',
-            }
-        ]
-    }
+    assert response.json() == {'users': []}
 
 
-def test_read_user_by_id(client):
+def test_read_users_with_user(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
+    response = client.get('/users/')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'users': [user_schema]}
+
+
+def test_read_user_by_id(client, user):
     response = client.get('/users/1')
 
     assert response.status_code == HTTPStatus.OK
@@ -50,14 +79,14 @@ def test_read_user_by_id(client):
     }
 
 
-def test_read_user_by_id_fail(client):
+def test_read_user_by_id_fail(client, user):
     response = client.get('/users/10')
 
     assert response.status_code == HTTPStatus.NOT_FOUND
     assert response.json() == {'detail': 'User not foud!'}
 
 
-def test_update_user(client):
+def test_update_user(client, user):
     response = client.put(
         '/users/1',
         json={
@@ -74,7 +103,7 @@ def test_update_user(client):
     }
 
 
-def test_update_user_fail(client):
+def test_update_user_fail(client, user):
     response = client.put(
         '/users/2',
         json={
@@ -89,13 +118,13 @@ def test_update_user_fail(client):
     assert response.json() == {'detail': 'User not foud!'}
 
 
-def test_delete_user(client):
+def test_delete_user(client, user):
     response = client.delete('/users/1')
 
     assert response.json() == {'message': 'User deleted!'}
 
 
-def test_delete_user_fail(client):
+def test_delete_user_fail(client, user):
     response = client.delete('/users/2')
 
     assert response.status_code == HTTPStatus.NOT_FOUND
